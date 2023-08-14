9951b38c29050ab95ff59aafedf23b8883da4fe0
Andallfor <leozwang2005@gmail.com>
2023-08-13 21:14:25 -0400

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["src/*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'home-1': "url('../src/mainBgPics/1.jpg')"
+      },
+    },
+  },
+  plugins: [],
+}
+
