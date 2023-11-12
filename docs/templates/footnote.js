773f6cb6b655e3f4950f8ef1e59195f35fef5aa3
Andallfor <leozwang2005@gmail.com>
2023-11-11 23:44:52 -0500

    blog

--- docs/templates/footnote.js
+++ docs/templates/footnote.js
@@ -1,15 +1,15 @@
 const createFootnote = (header, text, ref) => {
     return `
         <span class="peer">${ref}</span><!--
-     --><div id="footnote-note" class="absolute group">
+     --><div id="footnote-note" class="absolute group -z-10">
             <div class="relative w-[65vw] flex justify-start flex-row -z-10 -translate-y-6">
                 <div class="min-w-[calc(min(40vw,48rem)+2rem)]"></div>
                 <div>
                     <span class="relative">
                     <span class="block absolute -inset-1 -inset-x-2 -skew-y-1 bg-gradient-to-r transition-all duration-200 from-main-dark to-main-light peer-hover:group-[]:-ml-5 peer-hover:group-[]:-mr-5"></span>
-                        <span class="relative font-black text-stone-50">${header}</span><!--
+                        <span class="relative font-black text-stone-50 text-base">${header}</span><!--
                 --></span>
-                    <div class="font-extralight text-stone-800 text-base">${text}</div><!--
+                    <div class="font-extralight text-stone-800 text-base z-20">${text}</div><!--
             --></div>
             </div>
         </div><!--
@@ -42,7 +42,7 @@ const footnoteInit = () => {
         const t = ele.getAttribute('footnote-text');
         const r = ele.getAttribute('footnote-highlight');
 
-        ele.classList.add('underline', 'text-stone-950', 'decoration-main-light', 'font-semibold');
+        ele.classList.add('highlight', 'text-stone-950');
 
         ele.innerHTML += createFootnote(h, t, r);
 
