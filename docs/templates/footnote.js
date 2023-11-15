fc3e91cb8f44cb726d30ddc71c99a444bf6d385e
Andallfor <leozwang2005@gmail.com>
2023-11-15 15:49:10 -0500

    mobile footnotes

--- docs/templates/footnote.js
+++ docs/templates/footnote.js
@@ -1,16 +1,21 @@
 const createFootnote = (header, text, ref) => {
     return `
-        <span class="peer">${ref}</span><!--
-     --><div id="footnote-note" class="group -z-10 hidden absolute 2xl:block">
-            <div class="relative w-[65vw] flex justify-start flex-row -z-10 -translate-y-6"> <!-- left end of text to right end of screen -->
-                <div class="min-w-[calc(min(40vw,48rem)+2rem)]"></div> <!-- right end of text to right end of screen -->
-                <div>
+        <span class="peer" tabindex="1">${ref}</span><!--
+     --><div id="footnote-note" class="group xl:-z-10 z-50 xl:absolute fixed max-xl:bottom-0 max-xl:transition-opacity max-xl:duration-300 xl:opacity-100 max-xl:peer-focus:opacity-100 max-xl:peer-hover:opacity-100 opacity-0">
+            <!-- left end of text to right end of screen -->
+            <div class="absolute xl:relative w-[calc(min(48rem,calc(100vw-64px)))] xl:w-[calc(min(48rem,calc(100vw-64px))/2+50vw-32px)] flex justify-start flex-row -z-10 -translate-y-6">
+                <!-- right end of text to right end of screen -->    
+                <div class="xl:min-w-[calc(min(48rem,calc(100vw-64px))+1rem)]"></div>
+                <div class="-translate-y-full xl:translate-y-0">
                     <span class="relative">
-                    <span class="block absolute -inset-1 -inset-x-2 -skew-y-1 bg-gradient-to-r transition-all duration-200 from-main-dark to-main-light peer-hover:group-[]:-ml-5 peer-hover:group-[]:-mr-5"></span>
+                        <span class="block absolute -inset-1 -inset-x-2 -skew-y-1 bg-gradient-to-r transition-all duration-200 from-main-dark to-main-light xl:peer-hover:group-[]:-ml-3 xl:peer-hover:group-[]:-mr-3"></span>
                         <span class="relative font-black text-stone-50 text-base">${header}</span><!--
                 --></span>
-                    <div class="font-extralight text-stone-800 text-base z-20">${text}</div><!--
-            --></div>
+                    <div class="relative ml-2 -z-30 xl:z-20">
+                        <span class="block absolute -inset-y-3 -inset-x-2 max-xl:bg-stone-50 max-xl:border-2 border-main-light -z-10"></span>
+                        <span class="xl:font-extralight text-stone-800 text-xs sm:text-sm md:text-base font-light">${text}</span>
+                    </div>
+            </div>
             </div>
         </div><!--
 -->`;
@@ -23,14 +28,17 @@ const updateFootnotePositions = () => {
     elements.forEach((e) => {
         const element = e.querySelector("#footnote-note");
         element.style.marginTop = "0px";
-        let rect = element.getBoundingClientRect();
-        let t = rect.top + window.scrollY;
-        let b = rect.bottom + window.scrollY;
-        
-        let offset = t > currentBottom ? 0 : currentBottom - t + 25;
-        element.style.marginTop = offset + "px";
 
-        currentBottom = b + offset;
+        if (window.innerWidth >= 1280) {
+            let rect = element.getBoundingClientRect();
+            let t = rect.top + window.scrollY;
+            let b = rect.bottom + window.scrollY;
+            
+            let offset = t > currentBottom ? 0 : currentBottom - t + 25;
+            element.style.marginTop = offset + "px";
+
+            currentBottom = b + offset;
+        }
     });
 };
 
