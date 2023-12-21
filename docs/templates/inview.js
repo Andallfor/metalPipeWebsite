5c5f83bb5f74533cac4eefa74e8aa0a528257839
Andallfor <leozwang2005@gmail.com>
2023-12-21 02:30:53 -0500

    test main page style 1

--- docs/templates/inview.js
+++ docs/templates/inview.js
@@ -1,23 +1,15 @@
-const visHookElements = [];
-
-const updateVisHook = () => {
-    visHookElements.forEach((e) => {
-        // https://stackoverflow.com/questions/123999/how-can-i-tell-if-a-dom-element-is-visible-in-the-current-viewport
-        let rect = e.getBoundingClientRect();
-        let inView = rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-
-        e.classList.toggle('is-inview', inView);
+const updateVisHook = (entires, observer) => {
+    entires.forEach((e) => {
+        e.target.classList.toggle("is-inview", e.isIntersecting);
     });
 }
 
 const addVisHooks = () => {
     const hb = document.querySelectorAll("#visHook");
 
+    let observer = new IntersectionObserver(updateVisHook, {});
+
     hb.forEach((ele) => {
-        visHookElements.push(ele);
+        observer.observe(ele);
     });
-
-    updateVisHook();
-
-    addEventListener('scroll', (event) => {updateVisHook();});
 }
