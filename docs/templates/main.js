958e18f624a69f7d54854eef3a2adb3871ad9574
Andallfor <leozwang2005@gmail.com>
2023-12-29 04:37:19 -0500

    checkpoint

--- docs/templates/main.js
+++ docs/templates/main.js
@@ -1,12 +1,17 @@
-const createMainSection = (isRight, media, isImage, titleIntro, title, body) => {
+const createMainSection = (isRight, media, isImage, titleIntro, title, body, mobile) => {
+    if (mobile != null) media = mobile;
+
     const m = isImage ? 
         `<img src="${media}" class="w-[95%] object-contain">` : 
-        `<video playsinline autoplay loop muted class="w-[95%] object-contain"><source src="${media}" type="video/mp4"/></video>`;
+        `<video id="main-section-vid-id" muted class="w-[95%] object-contain"><source src="${media}" type="video/mp4"/></video>`;
 
     return `
     <div class="md:grid grid-cols-2">
         <div class="flex justify-center md:justify-start ${isRight ? "" : "md:order-last"}">
-            ${m}
+            <div class="flex justify-start flex-col">
+                ${m}
+                <div id="main-section-vid-hook"></div>
+            </div>
         </div>
         <div class="flex flex-col flex-start h-full">
             <div id="vis-hook" class="group vis-hook-perm">
@@ -45,13 +50,109 @@ const mainInit = () => {
         if (ele.hasAttribute('main-section-img')) {
             isImage = true;
             media = ele.getAttribute('main-section-img');
-        } else {
-            media = ele.getAttribute('main-section-vid')
+        } else media = ele.getAttribute('main-section-vid');
+
+        let mobile = null;
+        if (ele.hasAttribute('main-section-mobile') && window.screen.width <= 768) { // 768 is the md breakpoint
+            mobile = ele.getAttribute('main-section-mobile');
         }
+
         const ti = ele.getAttribute('main-section-intro');
         const t = ele.getAttribute('main-section-title');
         const b = ele.getAttribute('main-section-body');
 
-        ele.innerHTML += createMainSection(r, media, isImage, ti, t, b);
+        ele.innerHTML += createMainSection(r, media, isImage, ti, t, b, mobile);
+
+        if (ele.hasAttribute('main-section-vid-slider')) { // shush i dont want to hear it
+            const vid = ele.querySelector('#main-section-vid-id');
+            const hook = ele.querySelector('#main-section-vid-hook');
+
+            const thresholds = [17, 10, 35, 18, 20];
+
+            hook.innerHTML += `
+            <div class="w-full flex items-center relative flex-col">
+                <div id="main-section-vid-slider" class="relative w-[90%]"></div>
+
+                <div class="w-[87%] font-mono text-main-dark text-sm text-center flex gap-2 flex-row">
+                    <div class="w-[17%] h-2 border-b-2 border-l-2 border-r-2 border-main-dark flex justify-center relative">
+                        <span class="mt-4 absolute w-full">Active Intake</span>
+                    </div>
+                    <div class="w-[10%] h-2 border-b-2 border-l-2 border-r-2 border-main-dark flex justify-center relative">
+                        <span class="mt-4 absolute w-full">Extract Pixel</span>
+                    </div>
+                    <div class="w-[35%] h-2 border-b-2 border-l-2 border-r-2 border-main-dark flex justify-center relative">
+                        <span class="mt-4 absolute w-full">Extend Lift</span>
+                    </div>
+                    <div class="w-[18%] h-2 border-b-2 border-l-2 border-r-2 border-main-dark flex justify-center relative">
+                        <span class="mt-4 absolute w-full">Prepare Arm</span>
+                    </div>
+                    <div class="w-[20%] h-2 border-b-2 border-l-2 border-r-2 border-main-dark flex justify-center relative">
+                        <span class="mt-4 absolute w-full">Finish Extension</span>
+                    </div>
+                </div>
+            </div>
+            `;
+
+            const initValue = 75;
+            const vidTime = 5; // TODO: dont do this - but .seekable seems to only return 0 length?
+
+            const slider = new RangeSliderPips({
+                target: hook.querySelector('#main-section-vid-slider'),
+                props: {
+                    min: 0,
+                    max: 100,
+                    pips: true,
+                    pipstep: 1,
+                    springValues: {
+                        stiffness: 1,
+                        damping: 1,
+                    },
+                    values: [initValue],
+                    hoverable: false,
+                }
+            });
+
+            const pipHook = hook.querySelector('.rangePips');
+
+            const fillHook = hook.querySelector('.rangeSlider').appendChild(document.createElement('div'));
+            fillHook.classList.add('h-[8px]', 'bg-gradient-to-l', 'rounded-sm', 'from-main-light', 'to-main-dark');
+            fillHook.style.width = initValue + '%';
+
+            hook.querySelector('.rangeHandle').appendChild(document.createElement('div')).classList.add('rotate-45', 'translate-y-[3px]', 'bg-white', 'relative', '-z-10', 'h-[16px]', 'w-[16px]');
+
+            slider.$on('change', (e) => {
+                vid.currentTime = (e.detail.value / 100.0) * vidTime;
+                const v = e.detail.previousValue == e.detail.startValue ? 0 : e.detail.previousValue;
+
+                // new pips in range, toggle them
+                if (v == 0) {
+                    // v is 0 if we click to a new position - so we have to just check over everything (shush i cant be bothered)
+                    for (let i = 0; i < 100; i++) {
+                        if (i < e.detail.value) pipHook.children[i].classList.add('in-range-custom');
+                        else pipHook.children[i].classList.remove('in-range-custom');
+                    }
+                } else {
+                    if (v < e.detail.value) {
+                        for (let i = v; i < e.detail.value; i++) {
+                            pipHook.children[i].classList.add('in-range-custom');
+                        }
+                    } else {
+                        for (let i = v; i > e.detail.value; i--) {
+                            pipHook.children[i].classList.remove('in-range-custom');
+                        }
+                    }
+                }
+
+                fillHook.style.width = e.detail.value + '%';
+            });
+            
+
+            vid.currentTime = (initValue / 100.0) * vidTime;
+
+            for (let i = 0; i < 100; i++) {
+                if (i < initValue) pipHook.children[i].classList.add('in-range-custom');
+                else pipHook.children[i].classList.remove('in-range-custom');
+            }
+        }
     });
 }
